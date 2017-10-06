ec42ab285c9e776e27597b592cd4b4d8ba98f21f
Rasmus Munk <40161642@live.napier.ac.uk>
2017-10-06 15:27:17 +0200

    Added default envvars script

--- projects-envvars.py
+++ projects-envvars.py
@@ -8,10 +8,10 @@ os.environ['MAIL_PORT'] = '25'
 os.environ['MAIL_USE_TLS'] = 'True'
 os.environ['MAIL_USE_SSL'] = 'False'
 
-# Used to send authentication requests
-os.environ['MAIL_USERNAME'] = 'email'
-os.environ['MAIL_PASSWORD'] = 'password'
+# Used to send authentication requests -> authenticates against the above specified server
+os.environ['MAIL_USERNAME'] = 'replaceme'
+os.environ['MAIL_PASSWORD'] = 'replaceme'
 
 # Used to approve authentication requests received by MAIL_USERNAME
 # comma seperate if more should be included
-os.environ['ADMINS_EMAIL'] = 'authentication_grant_email'
+os.environ['ADMINS_EMAIL'] = 'replaceme'
