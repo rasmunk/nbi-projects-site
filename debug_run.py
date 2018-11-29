50e46297d09dc2ef24df22197b52d2f6ffda2e19
Rasmus Munk <munk1@live.dk>
2018-11-29 19:24:44 +0100

    Add reqiured modules to requirements

--- debug_run.py
+++ debug_run.py
@@ -1,17 +1,19 @@
 # Load enviroment variables
 exec(open("./projects-envvars.py").read())
+import datetime
 from projects import app
 from projects.models import User
-from bcrypt import hashpw
-import datetime
+from bcrypt import hashpw, gensalt
 
 if __name__ == '__main__':
     # Implement test user
     user = User.get_with_first('email', 'test@nbi.ku.dk')
     if user is None:
-        user = User(email='test@nbi.ku.dk', password=hashpw(bytes("test", 'utf-8'), bytes(app.config['SECURITY_PASSWORD_SALT'], 'utf-8')),
-                    projects=[], is_active=True, is_authenticated=True, is_anonymous=False,
+        user = User(email='test@nbi.ku.dk',
+                    password=hashpw(bytes("test", 'utf-8'),
+                                    gensalt()),
+                    projects=[], is_active=True,
+                    is_authenticated=True, is_anonymous=False,
                     confirmed_on=datetime.datetime.now())
         user.save()
     app.run(debug=True)
-
