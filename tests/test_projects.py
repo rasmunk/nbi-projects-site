c45b7c6548be3915e2aa166017e836370b3f39b9
Rasmus Munk <munk1@live.dk>
2019-12-18 13:54:59 +0100

    Update travis to multiple py3 version

---
+++ tests/test_projects.py
@@ -0,0 +1,45 @@
+import unittest
+import os
+from nbi import app
+from projects.models import Project
+
+
+class FairTestCase(unittest.TestCase):
+    def setUp(self):
+        app.testing = True
+
+        # Required folders
+        folders = {}
+        folders['DATA_FOLDER'] = app.config['DATA_FOLDER'] = os.path.join(
+            os.getcwd(), "data")
+        folders['UPLOAD_FOLDER'] = app.config['UPLOAD_FOLDER'] = os.path.join(
+            os.getcwd(), "images")
+        # Create required folders for the application if they don't exist
+        for key, folder in folders.items():
+            try:
+                os.makedirs(folder)
+                print("Created: " + folder)
+            except FileExistsError:
+                pass
+
+        app.config['WTF_CSRF_ENABLED'] = False
+        # Override default DB setting
+        # -> use a testing db instead of the default
+        app.config['DB'] = os.path.join(app.config['DATA_FOLDER'],
+                                        "dataset_test")
+        self.app = app.test_client()
+
+    def tearDown(self):
+        # Clean up
+        Project.clear()
+        self.assertTrue(len(Project.get_all()) == 0)
+
+    def test_dummy(self):
+        self.assertTrue(True)
+
+    def create_user(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
